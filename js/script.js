4f897c4167ad34d925f240b7e09206f3f9c1ad0c
swarley00 <jason.caryl@gmail.com>
2015-03-31 10:27:29 -0400

    Display input value in a list item

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
-	// var input = $(".input-field").val();
+	$('.add-btn').click(function(){
+		var listvalue = $('.input-field').val();
+		// var listitem = $('<li><div class="checkmark"><img src="images/checkmark.png"></div>
+		// 			      <p class="item selected">' + listvalue + '</p><button class="del-btn" type="button">DEL</button>
+		// 				  </li>');
+		// $('.shopping-items').append(listitem);
+		// alert(listitem);
+		$('.shopping-items').append("<li><p>" + listvalue + "</p></li>");
+	});
+
 	$(document).keydown(function(event){
+		var listitem = $('.input-field').val();
 		if (event.which == 13) {
-			$(".shopping-items").prepend("<p>milk</p>");
+			// alert(listitem);
+			// e.preventDefault();
+			// $('.shopping-items').append("<li><p>" + listitem + "</p></li>");
 		}
 	});
 });

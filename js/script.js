9e08203e011a2646f72d5733c455e76b024d9a1a
swarley00 <jason.caryl@gmail.com>
2015-04-02 12:24:57 -0400

    Add new class to the paragraph text when toggled on and remove it when toggled off.

--- js/script.js
+++ js/script.js
@@ -3,16 +3,28 @@ $(document).ready(function(){
 	var checkmarkBox = "<div class='checkmark'><img class='check' src='images/checkmark.png'></div>";
 	var delBtn = "<button class='del-btn' type='button'>DEL</button>";
 
+	//When the Add Button is clicked, a list item with the input field's value is added to the page.
 	$(".add-btn").click(function(){
 		var listvalue = $(".input-field").val();
 		shoppingList.append("<li>" + checkmarkBox + "<p class='item'>" + listvalue + "</p>" + delBtn + "</li>");
 	});
 
+	//When a list item is clicked, the checkmark image toggles on and the selected class is added to the paragraph text. If the toggle is clicked off the selected class is removed.
+	//1. Remove list items.
+	//2. Move the click off of the list item and only apply it to the check box.
 	shoppingList.on("click", "li", function(event){
 		$(this).find(".check").toggle();
-		// console.log(".check");
+		if ($(".check").is(":visible")) {
+			$(this).find(".item").addClass("selected");
+		} 
+		else {
+			$(this).find(".item").removeClass("selected");
+		} 
 	});
 
+	//Add the same functionality as the add button above, but with the enter key.
+	//After everything is working clean up the code by adding everything into a function and passing the function to each event.
+	//Also, make the code more efficient by making jQuery objects into variables and using the variables.
 	$(document).keydown(function(event){
 		var listitem = $('.input-field').val();
 		if (event.which == 13) {
@@ -20,19 +32,3 @@ $(document).ready(function(){
 		}
 	});
 });
-
-
-
-//Things I've tried for the click function
-// var listitem = $('<li><div class="checkmark"><img src="images/checkmark.png"></div>
-// 			      <p class="item selected">' + listvalue + '</p><button class="del-btn" type="button">DEL</button>
-// 				  </li>');
-// $('.shopping-items').append(listitem);
-// alert(listitem);
-
-
-//Things I've tried for the keydown function
-// e.preventDefault();
-// $('.shopping-items').append("<li><p>" + listitem + "</p></li>");
-
-//Toggle display on and off

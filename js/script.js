ab235b4402550fc18febcd07b604d2015cd5b768
swarley00 <jason.caryl@gmail.com>
2015-04-04 22:29:31 -0400

    Add list item when the enter key is pressed

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 	var shoppingList = $(".shopping-items");
 	var checkmarkBox = "<div class='checkmark'><img class='check' src='images/checkmark.png'></div>";
 	var delBtn = "<button class='del-btn' type='button'>DEL</button>";
+	var addBtn = $(".add-btn");
 
 	//When the Add Button is clicked, a list item with the input field's value is added to the page.
-	$(".add-btn").click(function(){
-		var listvalue = $(".input-field").val();
-		shoppingList.append("<li>" + checkmarkBox + "<p class='item'>" + listvalue + "</p>" + delBtn + "</li>");
+	addBtn.click(function(){
+		addItem();
 	});
 
 	//When a list item is clicked, the checkmark image toggles on and the selected class is added to the paragraph text. If the toggle is clicked off the selected class is removed.
@@ -20,18 +20,26 @@ $(document).ready(function(){
 		} 
 	});
 
-	//When the delete button is clicked on a list item remove that list item.
+	//When the Delete Button is clicked on a list item remove that list item.
 	shoppingList.on("click", ".del-btn", function(event){
 		$(this).closest("li").remove();
 	});
 
-	//Add the same functionality as the add button above, but with the enter key.
-	//After everything is working clean up the code by adding everything into a function and passing the function to each event.
-	//Also, make the code more efficient by making jQuery objects into variables and using the variables.
+	//When the Enter Key is pressed a new list item is created with the input field's value.
 	$(document).keydown(function(event){
 		var listitem = $('.input-field').val();
 		if (event.which == 13) {
-			alert(listitem);
+			event.preventDefault();
+			addItem();
 		}
 	});
 });
+
+//When called above, adds the list item with the input field's value.
+function addItem() {
+	var shoppingList = $(".shopping-items");
+	var checkmarkBox = "<div class='checkmark'><img class='check' src='images/checkmark.png'></div>";
+	var delBtn = "<button class='del-btn' type='button'>DEL</button>";
+	var listvalue = $(".input-field").val();
+	shoppingList.append("<li>" + checkmarkBox + "<p class='item'>" + listvalue + "</p>" + delBtn + "</li>");
+}
